fb07b4be882a11a66ca36187ad298860c8e9ef7a
OmarHabeb <omar.momen.df@gmail.com>
2024-08-17 01:41:36 +0300

    login_page edit

--- lib/Screens/LoginPage.dart
+++ lib/Screens/LoginPage.dart
@@ -1,5 +1,6 @@
 import 'package:drugstore/Screens/CategoriesPage.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -16,153 +17,157 @@ class _LoginPAgeState extends State<LoginPage> {
     TextEditingController Second = TextEditingController();
 
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("Images/d.jpg"), fit: BoxFit.cover)),
-        child: Center(
-          child: Container(
-            width: 300,
-            height: 350,
-            decoration: BoxDecoration(
-              color: Colors.blue.withOpacity(0.5),
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF00B3B6),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30)),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "LogIn Page",
-                      style: TextStyle(color: Colors.black, fontSize: 25),
-                    ),
-                  ),
+      body:  Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Lottie.asset("Images/login.json" , repeat: true),
+          ),
+          Center(
+              child: Container(
+                width: 300,
+                height: 350,
+                decoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-                Container(
-                  padding: EdgeInsets.all(15),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
-                        child: TextField(
-                          controller: Second,
-                          decoration: InputDecoration(
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide.none),
-                            hintText: "Enter Your E-Mail",
-                            prefixIcon: Icon(
-                              Icons.person,
-                              color: Colors.black,
-                            ),
-                          ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF00B3B6),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "LogIn Page",
+                          style: TextStyle(color: Colors.black, fontSize: 25),
                         ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
-                        child: TextField(
-                          controller: Frist,
-                          decoration: InputDecoration(
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide.none),
-                            hintText: "Enter Your Password",
-                            prefixIcon: Icon(
-                              Icons.key,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      child: Column(
                         children: [
-                          Row(
-                            children: [
-                              Container(
-                                width: 20,
-                                height: 10,
-                                child: Checkbox(
-                                  value: index,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      index = value!;
-                                    });
-                                  },
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.black))),
+                            child: TextField(
+                              controller: Second,
+                              decoration: InputDecoration(
+                                border: UnderlineInputBorder(
+                                    borderSide: BorderSide.none),
+                                hintText: "Enter Your E-Mail",
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Colors.black,
                                 ),
                               ),
-                              Text("Remember me"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.black))),
+                            child: TextField(
+                              controller: Frist,
+                              decoration: InputDecoration(
+                                border: UnderlineInputBorder(
+                                    borderSide: BorderSide.none),
+                                hintText: "Enter Your Password",
+                                prefixIcon: Icon(
+                                  Icons.key,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 20,
+                                    height: 10,
+                                    child: Checkbox(
+                                      value: index,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          index = value!;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  Text("Remember me" ,style: TextStyle(fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                              InkWell(
+                                child: Text("Forget Password?",style: TextStyle(fontWeight: FontWeight.bold)),
+                              )
                             ],
                           ),
+                          SizedBox(
+                            height: 20,
+                          ),
                           InkWell(
-                            child: Text("Forget Password? "),
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => CategoriesPage()));
+                            },
+                            child: Container(
+                              width: 120,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: Color(0xFF00B3B6),
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: Center(
+                                  child: Text(
+                                "LogIn",
+                                style: TextStyle(color: Colors.black, fontSize: 20),
+                              )),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Don't have an account"),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Sin Up",
+                                    style: TextStyle(color: Colors.blue),
+                                  ))
+                            ],
                           )
                         ],
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => CategoriesPage()));
-                        },
-                        child: Container(
-                          width: 120,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Color(0xFF00B3B6),
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Center(
-                              child: Text(
-                            "LogIn",
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                          )),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account"),
-                          TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                "SinUp",
-                                style: TextStyle(color: Colors.white),
-                              ))
-                        ],
-                      )
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+        ],
       ),
+      
     );
   }
 }
