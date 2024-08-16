c1a8af7abbc1e0033d2d47de2a34404db445f10b
OmarHabeb <omar.momen.df@gmail.com>
2024-08-16 17:52:51 +0300

    Initial commit

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,33 @@
+import 'package:drugstore/LoginPage.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SlpashScreenState();
+}
+class _SlpashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              "Images/Doctor_Splash.json",
+              repeat: false,
+              onLoaded: (p0) => Future.delayed(p0.duration, () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => LoginPage()));
+              }),
+            ),
+            Text("Welcome In Your Drugstore" , style: TextStyle(color: Colors.white , fontSize: 20 ,fontWeight: FontWeight.bold),)
+          ],
+        ),
+      
+    );
+  }
+}
